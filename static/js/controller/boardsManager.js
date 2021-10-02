ee69e5521808f959729a31874803157ab6894191
tamasstephen <kicsitnagyon@gmail.com>
2021-10-02 11:44:37 +0200

    Implement behaviour on clicking outside the input and escape hits
    Clicking outside the input field, hitting escape or pressing enter
    when the input field is empty removes the new board from the #root
    element.

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -27,6 +27,7 @@ function showHideButtonHandler(clickEvent) {
 }
 
 function addNewBoard(){
+
   const boardBuilder = htmlFactory(htmlTemplates.board);
   const board = boardBuilder({"title": "", "id": "pending_board"});
   domManager.addChild("#root", board);
@@ -38,25 +39,48 @@ function nameNewBoard(){
   const headline = document.querySelector('div[data-board-id="pending_board"]');
   headline.innerHTML = `<input type="text" name="board_name" id="name_new_board">`
   const input = document.querySelector("#name_new_board");
-  input.addEventListener("keypress", handleInputSaveName)
+  input.addEventListener("keydown", handleInputSaveName);
+  input.focus();
+  wait(100).then(()=> document.body.addEventListener('click', clickOutside) )
 }
 
 function handleInputSaveName(e){
+  const board = document.querySelector('div [data-board-id="pending_board"]');
+  if (e.key === "Escape"){
+    removeBoard(board);
+  }
   if(e.key === "Enter"){
    const newName = e.currentTarget.value;
-   const board = document.querySelector('div [data-board-id="pending_board"]')
    const button = document.querySelector('button[class="toggle-board-button"][data-board-id="pending_board"]')
    // const input = document.querySelector("#name_new_board");
-   // console.log(input.closest("div")); // TODO: ask why it does not work if I save the current target
+   // console.log(input.closest("div")); // TODO: ask why it does not work if I save the current target, but it does if I pick it with the querySelector
    if (newName.length < 1 ){
-     console.log("hey!");
+     removeBoard(board);
    } else {
      board.textContent = newName;
      board.dataset.boardId = "dummy";
      button.dataset.boardId = "dummy";
      e.currentTarget.remove();
+     document.body.removeEventListener("click", clickOutside);
    }
   }
 }
 
+function removeBoard(board){
+  const parentDiv = board.closest(".board-container");
+  parentDiv.remove();
+  document.body.removeEventListener("click", clickOutside);
+}
 
+function clickOutside(e) {
+  const input = document.querySelector("#name_new_board");
+  if (e.target !== input) {
+    const boardContainer = input.closest(".board-container");
+    boardContainer.remove();
+    document.body.removeEventListener("click", clickOutside);
+  }
+}
+
+function wait(ms){
+  return new Promise((resolve) => { return setTimeout(resolve, ms) });
+}
