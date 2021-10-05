1bf9dd6b03613e895129d63dda4d715b4dd18795
tamasstephen <kicsitnagyon@gmail.com>
2021-10-05 11:35:04 +0200

    fix: Fix merge conflict bug where cards were not loaded

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -8,6 +8,7 @@ export let cardsManager = {
     for (let card of cards) {
       const cardBuilder = htmlFactory(htmlTemplates.card);
       const content = cardBuilder(card);
+      console.log("i'm here")
       domManager.addChild(`.status-col[data-status-id="${card["status_id"]}"][data-board-id="${boardId}"]`, content);
       domManager.addEventListener(
         `.card[data-card-id="${card.id}"]`,
